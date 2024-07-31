9b3f6c43e9a1229d34d052fc764759146e57b589
Elias Entrup <elias.entrup@tib.eu>
2024-07-31 13:52:31 +0200

    Add results for soil science and education

--- datasets.py
+++ datasets.py
@@ -32,5 +32,26 @@ def get_articles(key='infectious_diseases'):
         'Journal of the Mechanical Behavior of Biomedical Materials', 
         'Journal of Materials in Civil Engineering', 
 
+        'Educational Psychology', 'CBE Life Sciences Education',
+        'Interactive Learning Environments', 'Education and Information Technologies',
+        'Early Education and Development', 'Contemporary Educational Psychology',
+        'Computers and Education', 'Journal of Educational Psychology',
+        'British Journal of Educational Technology', 'Higher Education',
+        'Journal of School Psychology', 'British Journal of Educational Psychology',
+        'Educational Technology Research and Development', 'Teaching and Teacher Education',
+        'Learning and Individual Differences',
+        'Measurement: Journal of the International Measurement Confederation',
+        'European Journal of Psychology of Education', 'Reading and Writing',
+        'Social Psychology of Education', 'Nurse Education Today',
+
+        'Soil Biology and Biochemistry', 'Geoderma', 'Soil and Tillage Research',
+        'Biology and Fertility of Soils', 'Urban Forestry and Urban Greening',
+        'Agricultural Water Management', 'Molecular Plant Pathology',
+        'Field Crops Research', 'Environmental Technology and Innovation',
+        'Applied Soil Ecology', 'Solid Earth',
+        'Soil Dynamics and Earthquake Engineering', 'European Journal of Agronomy',
+        'European Journal of Soil Science', 'Land Degradation and Development',
+        'Plant and Soil', 'European Journal of Soil Biology',
+        'Soil Use and Management', 'Biosystems Engineering', 'Applied Clay Science',
     ])].reset_index(drop=True)
     return df
